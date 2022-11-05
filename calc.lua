0045fe161382a0323443eaa1ff176b41b60d9075
folcred <robttheb@gmail.com>
2022-11-05 14:33:23 -0600

    Fixed some comment grammer.

--- calc.lua
+++ calc.lua
@@ -1,11 +1,11 @@
 -- Making a simple CLI calculator
 
--- Get the user input
+-- Get the user input.
 print("Enter a calculation:")
 local myString = io.read()
-local ops = { "+", "-", "*", "/" }
 
--- Seperate the operation from the numbers
+-- Seperate the operation from the numbers.
+local ops = { "+", "-", "*", "/" }
 for i, _ in ipairs(ops) do
   if string.find(myString, ops[i]) ~= nil then
     Loc = string.find(myString, ops[i])
@@ -13,21 +13,21 @@ for i, _ in ipairs(ops) do
   end
 end
 
--- Get and assign the numbers to variables
--- Wrapping it in a function is required for error checking
+-- Extract the numbers and assign them to variables.
+-- (Wrapping it in a function is required for error checking.)
 local function getNums()
   A = string.sub(myString, 1, (Loc - 1))
   B = string.sub(myString, (Loc + 1), -1)
 end
 
--- Check that the user entered a valid operator
+-- Check that the user entered a valid operator.
 if pcall(getNums) then
   print()
 else
   print("Invalid operator! Try again")
 end
 
--- Perform the match and return the result
+-- Perform the math and return the result.
 if Op == "-" then
    print("Result: " .. A - B)
 elseif Op == "+" then
