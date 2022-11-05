b791b240413dd642746aa4ea6054fab1a2c361ca
folcred <robttheb@gmail.com>
2022-11-05 11:44:25 -0600

    Refactored math operation and added error handling
    Instead of using a bunch of seperate if statements, I combined them
    in a single one. Added error checking in case the user entered
    something other than basic math operations. Still need to work on
    Lua error handling to come up with a better solution.

--- calc.lua
+++ calc.lua
@@ -2,7 +2,7 @@
 
 print("Enter a calculation:")
 local myString = io.read()
-local ops = {"+", "-", "*", "/"}
+local ops = { "+", "-", "*", "/" }
 
 for i, _ in ipairs(ops) do
   if string.find(myString, ops[i]) ~= nil then
@@ -11,16 +11,25 @@ for i, _ in ipairs(ops) do
   end
 end
 
-local a = string.sub(myString, 1, (Loc - 1))
-local b = string.sub(myString, (Loc + 1), -1)
+if Op == nil then
+   error("Invalid operation! Try again.")
+end
 
-if Op == "-" then print("Result: " .. a - b) end
-if Op == "+" then print("Result: " .. a + b) end
-if Op == "*" then print("Result: " .. a * b) end
-if Op == "/" then print("Result: " .. a / b) end
+A = string.sub(myString, 1, (Loc - 1))
+B = string.sub(myString, (Loc + 1), -1)
+
+if Op == "-" then
+   print("Result: " .. A - B)
+elseif Op == "+" then
+   print("Result: " .. A + B)
+elseif Op == "*" then
+   print("Result: " .. A * B)
+elseif Op == "/" then
+   print("Result: " .. A / B)
+end
 
 --[[
-    There's probably a better way to do this, but I'm happy with my first attempt
-    with my limited knowledge so far. I will revisit this as I learn, see if I come
-    up with a better way.
+   TODO: Need to figure out how to suppress the error traceback, make
+   a cleaner error message that wouldn't end up just confusing
+   a user.
 --]]
