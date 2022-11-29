30e19e755606768d2a7f1970a8ab830819d468a5
folcred <robttheb@gmail.com>
2022-11-29 15:27:30 -0700

    Made program loop unless the user enters quit key
    Wrapped the program in a loop so the user doesn't have to keep restarting
    it to do more math. Added and exit key they can press instead of an operation
    to quit the program. Makes it more useful as it will stay open while in use.

--- calc.lua
+++ calc.lua
@@ -1,39 +1,44 @@
 -- Making a simple CLI calculator
 
--- Get the user input.
-print("Enter a calculation:")
-local myString = io.read()
+-- Loop the program so the user doesn't have to restart each time
+while (true) do
+-- Get the user input, and tell how to exit.
+  print("Enter a calculation, \'q\' to exit:")
+  local myString = io.read()
 
--- Seperate the operation from the numbers.
-local ops = { "+", "-", "*", "/" }
-for i, _ in ipairs(ops) do
-  if string.find(myString, ops[i]) ~= nil then
-    Loc = string.find(myString, ops[i])
-    Op = ops[i]
+-- Set what to look for, the operation or the exit key.
+  local ops = { "+", "-", "*", "/", "q" }
+
+-- Find which they entered, an operation or exit.
+  for i, _ in ipairs(ops) do
+    if string.find(myString, ops[i]) ~= nil then
+      Loc = string.find(myString, ops[i])
+      Op = ops[i]
+-- Check for the exit key, and if there, quit.
+      if Op == "q" then
+        os.exit()
+      end
+    end
   end
-end
 
--- Extract the numbers and assign them to variables.
--- (Wrapping it in a function is required for error checking.)
-local function getNums()
-  A = string.sub(myString, 1, (Loc - 1))
-  B = string.sub(myString, (Loc + 1), -1)
-end
+  -- Extract the numbers and assign them to variables.
+  -- (Wrapping it in a function is required for error checking.)
+  local function getNums()
+    A = string.sub(myString, 1, (Loc - 1))
+    B = string.sub(myString, (Loc + 1), -1)
+  end
 
--- Check that the user entered a valid operator.
-if pcall(getNums) then
-  print()
-else
-  print("Invalid operator! Try again")
-end
+  -- Check that the user entered a valid operator.
+  if pcall(getNums) then
+    print()
+  else
+    print("Invalid operator! Try again")
+  end
 
--- Perform the math and return the result.
-if Op == "-" then
-   print("Result: " .. A - B)
-elseif Op == "+" then
-   print("Result: " .. A + B)
-elseif Op == "*" then
-   print("Result: " .. A * B)
-elseif Op == "/" then
-   print("Result: " .. A / B)
+  -- Perform the math and return the result.
+  if Op == "-" then print("Result: " .. A - B .. "\n")
+  elseif Op == "+" then print("Result: " .. A + B .. "\n")
+  elseif Op == "*" then print("Result: " .. A * B .. "\n")
+  elseif Op == "/" then print("Result: " .. A / B .. "\n")
+  end
 end
