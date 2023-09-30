671eef25a7de3d01dca03b5d66bd2a74fbccbff4
folcred <robttheb@gmail.com>
2023-09-30 07:21:11 -0600

    Meters to feet converter redo / refactor
    Actually did this some while ago, don't know why I didn't push it
    up but I didn't. Done now.

--- m2f.lua
+++ m2f.lua
@@ -1,30 +1,31 @@
 #!/usr/bin/lua
 
 function ToFeet(distance)
-    local feet = distance * 3.2808
-    print("\nIs " .. tonumber(string.format("%.2f", feet)) .. " feet.")
+	local feet = distance * 3.2808
+	print("\nIs " .. tonumber(string.format("%.2f", feet)) .. " feet.")
 end
 
 function ToMeters(distance)
-    local meters = distance * 0.3048
-    print("\nIs " .. tonumber(string.format("%.2f", meters)) .. " meters.")
+	local meters = distance * 0.3048
+	print("\nIs " .. tonumber(string.format("%.2f", meters)) .. " meters.")
 end
 
-print("Enter distance as \'32m\' or \'12f\' to convert 32 meters or 12 feet: ")
+print("Enter distance as '32m' or '12f' to convert 32 meters or 12 feet: ")
 local conversion = io.read()
-local conv_value = {"m", "f"}
+local conv_value = { "m", "f" }
 
 for i, _ in ipairs(conv_value) do
-    if string.find(conversion, conv_value[i]) ~= nil then
-        local conv_from = string.find(conversion, conv_value[i])
-        OriginalDistance = string.sub(conversion, 1, (conv_from - 1))
-        OriginalMeasure = string.sub(conversion, conv_from)
-    end
+	if string.find(conversion, conv_value[i]) ~= nil then
+		local conv_from = string.find(conversion, conv_value[i])
+		OriginalDistance = string.sub(conversion, 1, (conv_from - 1))
+		OriginalMeasure = string.sub(conversion, conv_from)
+	end
 end
 
 if OriginalMeasure == "m" then
-    ToFeet(OriginalDistance)
+	ToFeet(OriginalDistance)
 elseif OriginalMeasure == "f" then
-    ToMeters(OriginalDistance)
-else print("You didn\'t enter a proper conversion, try again.")
+	ToMeters(OriginalDistance)
+else
+	print("You didn't enter a proper conversion, try again.")
 end
