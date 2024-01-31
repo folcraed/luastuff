e93f9bcf435d2559fb242f240d142c13ba237f0e
folcred <robttheb@gmail.com>
2024-01-31 11:38:43 -0700

    Removed misc.lua, new lesson is exercise
    Changed misc.lua to iterator.lua, I want to work
    on that more to see how it's working, and how
    it produces the results. Going to use exercise.lua
    for lessons.

--- exercise.lua
+++ exercise.lua
@@ -1,32 +1,26 @@
-math.randomseed(os.time())
-Number = math.random(1, 100)
-
-Player = {}
-Player.guess = 0
-
-while Player.guess ~= Number do
-    print("Guess a number between 1 and 100")
-    Player.answer = io.read()
-    Player.guess = tonumber(Player.answer)
-    if Player.guess > Number then
-        print("Too high")
-    elseif Player.guess < Number then
-        print("Too low")
-    else
-        print("You guessed it!")
-        -- os.exit()
-    end
+Zombie = {}
+function Zombie.init(h, w, s, l)
+    local self = setmetatable({}, Zombie)
+    self.height = h
+    self.weight = w
+    self.speed = s
+    self.location = l
+    return self
 end
 
---[[
-    This was an exercise from the Lua learning book
-    by Seth Kenlon. I was wondering why he used a
-    table to store the player's guesses. So I did
-    a print of what was in the table at the end of
-    the game. It only has the last guess and the
-    actual number. So I'm not sure why he used a
-    table, it doesn't seem to add anything useful.
-]]
-for k, v in pairs(Player) do
-    print(k, v)
+-- Use the metatable
+-- TODO Change this to accept user input to create several zombies
+function Setup()
+    Z1 = Zombie.init(176, 50, "slow", "Forbes & Murray Avenue")
 end
+
+function Run()
+    print(Z1.location .. ": " ..
+        Z1.height .. " cm, " ..
+        Z1.weight .. "kg, " ..
+        Z1.speed)
+end
+
+-- Will need to change this when accepting user input.
+Setup()
+Run()
