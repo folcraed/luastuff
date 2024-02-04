d8e935a8130979e6c78c533426488420a4acc66c
folcred <robttheb@gmail.com>
2024-02-04 13:59:15 -0700

    Converted tabs to spaces, for reasons
    Just realized my editor was using tabs for indent, which Lua doesn't
    really recommend. Changed the setting for Lua files, should be good now.

--- c2f.lua
+++ c2f.lua
@@ -10,14 +10,14 @@ then outputs the result.
 
 --- Converts input fahrenheit to celsius
 function ToCelsius(fahrenheit)
-	local temp = ((5 / 9) * (fahrenheit - 32))
-	print("\nThat is " .. math.floor(temp) .. " celsius.")
+    local temp = ((5 / 9) * (fahrenheit - 32))
+    print("\nThat is " .. math.floor(temp) .. " celsius.")
 end
 
 --- Converts input celsius to fahrenheit
 function ToFahrenheit(celsius)
-	local temp = ((celsius * 9 / 5) + 32)
-	print("\nThat is " .. math.floor(temp) .. " fahrenheit.")
+    local temp = ((celsius * 9 / 5) + 32)
+    print("\nThat is " .. math.floor(temp) .. " fahrenheit.")
 end
 
 -- Get the user's input and see what type conversion they want.
@@ -26,18 +26,18 @@ local conversion = io.read()
 local temp_val = { "c", "f" }
 
 for i, _ in ipairs(temp_val) do
-	if string.find(conversion, temp_val[i]) ~= nil then
-		Location = string.find(conversion, temp_val[i])
-		Temperature = string.sub(conversion, 1, (Location - 1))
-		From_val = string.sub(conversion, -1)
-	end
+    if string.find(conversion, temp_val[i]) ~= nil then
+        Location = string.find(conversion, temp_val[i])
+        Temperature = string.sub(conversion, 1, (Location - 1))
+        From_val = string.sub(conversion, -1)
+    end
 end
 
 -- Do the conversion based on what they entered.
 if From_val == "c" then
-	ToFahrenheit(Temperature)
+    ToFahrenheit(Temperature)
 elseif From_val == "f" then
-	ToCelsius(Temperature)
+    ToCelsius(Temperature)
 else
-	print("You didn't enter a conversion, try again.")
+    print("You didn't enter a conversion, try again.")
 end
