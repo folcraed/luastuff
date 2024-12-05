46bfe5633bbc43216d9ea06511bae6471f3649ac
folcred <robttheb@gmail.com>
2024-12-05 16:21:27 -0700

    Added a test for nil value
    The Lua LSP showed my original code lacked a test for nil in the
    GPU read value, so added one. The formatter also made some fixups.

--- temps.lua
+++ temps.lua
@@ -9,20 +9,24 @@ local CPUFile = "/sys/class/thermal/thermal_zone2/temp"
 
 -- We use a function to extract the temperature to avoid file permission problems
 function ReadCPU()
-  local f = open(CPUFile)
-  if f then
-    CPU = tostring(f:read("a"))
-    f:close()
-  else
-    print("Can't read temperature file.")
-  end
-  return CPU
+	local f = open(CPUFile)
+	if f then
+		CPU = tostring(f:read("a"))
+		f:close()
+	else
+		print("Can't read temperature file.")
+	end
+	return CPU
 end
 
-print(string.format('%i', tonumber(ReadCPU() / 1000)) .. "ºC CPU")
+print(string.format("%i", tonumber(ReadCPU() / 1000)) .. "ºC CPU")
 
 -- Then we get the GPU temperature using lm-sensors and some CLI tools
 local GPUFile = io.popen("sensors | grep edge | cut -c16-17", "r")
-TheGPU = GPUFile:read("n")
+if GPUFile == nil then
+	TheGPU = 0
+else
+	TheGPU = GPUFile:read("n")
+end
 
 print(TheGPU .. "ºC GPU")
