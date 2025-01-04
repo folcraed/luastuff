3df3fe91be8a5eeefe067f10953be4d2000a62fd
folcred <robttheb@gmail.com>
2025-01-04 14:52:03 -0700

    Changed floored values to 1 decimal place
    Instead of using math.floor to round down, changed output to use
    one decimal place for more accurate results.

--- average.lua
+++ average.lua
@@ -8,15 +8,15 @@ local theNumbers = io.read()
 local counter = string.len(theNumbers)
 
 while counter > 0 do
-    if string.find(theNumbers, delim) ~= nil then
-      Loc = string.find(theNumbers, delim)
-    end
-      Number = string.sub(theNumbers, 1, (Loc -1))
-      CutMe = string.sub(theNumbers, 1, Loc)
-      theNumbers = string.gsub(theNumbers, CutMe, "")
-      total = total + tonumber(Number)
-      counter = counter - string.len(CutMe)
-      how_many = how_many + 1
+	if string.find(theNumbers, delim) ~= nil then
+		Loc = string.find(theNumbers, delim)
+	end
+	Number = string.sub(theNumbers, 1, (Loc - 1))
+	CutMe = string.sub(theNumbers, 1, Loc)
+	theNumbers = string.gsub(theNumbers, CutMe, "")
+	total = total + tonumber(Number)
+	counter = counter - string.len(CutMe)
+	how_many = how_many + 1
 end
 
-print("The average is: " .. math.floor(total / how_many))
+print(string.format("The average is: %.2f", (total / how_many)))
